e57c28c841f311b41a54e88b59a19ea0a960ae0d
Sebastian Callh <sebastian.callh@gmail.com>
2022-07-30 12:08:33 +0200

    Rename components.jl -> component.jl

--- src/TuringSTS.jl
+++ src/TuringSTS.jl
@@ -3,7 +3,7 @@ using LinearAlgebra
 
 export Seasonal, LocalLinear
 
-include("components/components.jl")
+include("components/component.jl")
 include("components/seasonal.jl")
 include("components/local_linear.jl")
 end
