0cc6b0bfed0627dcb0bfb045bef12052e084aa87
Sebastian Callh <sebastian.callh@gmail.com>
2022-07-30 12:49:13 +0200

    Update local linear seasonal toy to recover params

--- toys/local_linear_seasonal.jl
+++ toys/local_linear_seasonal.jl
@@ -1,7 +1,7 @@
 using StatsPlots, Turing, Distributions, LinearAlgebra, Statistics
 using ReverseDiff, Memoization
 using Random
-using BlockDiagonals
+using TuringSTS
 
 Random.seed!(1234)
 Turing.setadbackend(:reversediff)
@@ -21,7 +21,7 @@ function blockdiagonal(mats::Vector{Matrix{T}}) where T
     res
 end
 
-
+##
 function true_fn(t, x₀, level_drift_scale, slope_drift_scale, season_drift_scale, season_length, num_seasons, σ)
     local_lin_trans = [
         1 1;
@@ -39,7 +39,7 @@ function true_fn(t, x₀, level_drift_scale, slope_drift_scale, season_drift_sca
 
     loclin_drift_scale = vcat(level_drift_scale, slope_drift_scale)
     mapreduce(vcat, t) do t
-        new_season = mod(t-1, season_length) == 0
+        new_season = mod(t, season_length) == 1 && 1 < t
         seasonal_trans_ = new_season ? seasonal_trans : diagm(ones(4))
         seasonal_drift_scale_ = repeat([new_season ? season_drift_scale : 0], num_seasons)
         trans = blockdiagonal([local_lin_trans, seasonal_trans_])
@@ -50,67 +50,52 @@ function true_fn(t, x₀, level_drift_scale, slope_drift_scale, season_drift_sca
     end
 end
 
-T = 80
+function generate(tt, x, sts, σ)
+    mapreduce(vcat, tt) do t
+        y, x′, ϵ = sts(x, t)
+        x = x′ .+ ϵ.*randn(size(x))
+        y .+ σ.*randn()
+    end
+end
+
+T = 24
 σ = 0.1
 tt = collect(1:T)
-loc_lin_init = [1.3, -0.05]
+loc_lin_init = [0.8, -0.05]
 season_effects = [1, 2, 0, -2]
 x₀ = vcat(loc_lin_init, season_effects)
 num_seasons = length(season_effects)
 season_length = 4
-seasons = repeat(collect(1:num_seasons), inner=season_length, outer=div(T, num_seasons * season_length))
+seasons = repeat(collect(1:num_seasons), inner=season_length, outer=num_seasons)[1:T]
 level_drift_scale = 0.1
 slope_drift_scale = 0.05
 season_drift_scale = 0.1
-y = true_fn(tt, x₀, level_drift_scale, slope_drift_scale, season_drift_scale, season_length, num_seasons, σ)
-scatter(tt, y, color=seasons)
+sts = LocalLinear(level_drift_scale, slope_drift_scale) + Seasonal(num_seasons, season_length, season_drift_scale)
+y = generate(tt, x₀, sts, σ)
+scatter(tt, y, color=seasons, label=nothing, title="Data")
 
-@model function local_linear_seasonal(
-    T, num_seasons;
-    obs_noise_scale=1.,
-    level_scale=1.,
-    slope_scale=1.,
-    season_length=1,
-    season_drift_scale=1.,
-    forecast=0,
-)
-    obs = [1 0 1 0 0 0]
-    loclin_trans = [
-        1 1;
-        0 1
-    ]
-    loclin_scales = vcat(level_scale, slope_scale)
-    seasonal_trans = [
-        0 1 0 0;
-        0 0 1 0;
-        0 0 0 1;
-        1 0 0 0;
-    ]
-    D = size(obs, 2)
-    season_eltype = eltype(season_drift_scale)
-
+##
+@model function sts_model(T, sts; forecast=0)
+    D = latent_size(sts)
     x₀ ~ MvNormal(zeros(D), I)
-    x_trans_noise ~ filldist(Normal(0, 1), D, T)
-    σ ~ truncated(Normal(0, obs_noise_scale); lower=0)
+    z ~ filldist(Normal(0, 1), D, T)
+    σ ~ truncated(Normal(0, 0.1); lower=0)
     
     x = x₀
     T′ = T + forecast
     μ = map(1:T′) do t
-        new_season = mod(t-1, season_length) == 0
-        ses_trans = new_season ? seasonal_trans : diagm(ones(eltype(seasonal_trans), 4))
-        ses_scale = new_season ? vcat(season_drift_scale, zeros(season_eltype, num_seasons-1)) : zeros(season_eltype, num_seasons) 
-
-        trans = blockdiagonal([loclin_trans, ses_trans])
-        x_trans_scale = vcat(loclin_scales, ses_scale)
-        x = trans * x .+ x_trans_scale .* x_trans_noise[:,min(T, t)]
-        only(obs * x)
+        y, x′, ϵ = sts(x, t)
+        x = x′ .+ ϵ .* z[:,min(T, t)]
+        only(y)
     end
 
     Σ = I*σ^2
     y ~ MvNormal(μ, Σ)
 end
 
-model = local_linear_seasonal(T, num_seasons; level_scale=0.05, slope_scale=0.05, season_drift_scale=0.1, season_length, obs_noise_scale=0.1)
+level_scale, slope_scale = 0.1, 0.1
+sts = LocalLinear(level_scale, slope_scale) + Seasonal(num_seasons, season_length, season_drift_scale)
+model = sts_model(T, sts)
 prior_predictive = mapreduce(i -> rand(model).y, hcat, 1:50)
 prior_plt = plot(tt, prior_predictive, label=nothing, color=1, title="Prior predictive check")
 scatter!(prior_plt, tt, y, color=seasons, label="Data")
@@ -125,10 +110,11 @@ post_predictive = Array(predict(model, chain))
 @assert isapprox(mean(chain, "x₀[6]"), x₀[6]; atol=0.5)
 @assert isapprox(mean(chain, :σ), σ; atol=0.05)
 
+## 
 posterior_plt = plot(tt, post_predictive', color=1, alpha=0.1, label=nothing, title="Posterior predictive check")
 scatter!(posterior_plt, tt, y, label="Data", color=seasons)
 
-steps = 15
-y_forecast = Array(predict(local_linear_seasonal(T, num_seasons; level_scale=0.05, slope_scale=0.05, season_drift_scale=0.1, season_length, obs_noise_scale=0.1, forecast=steps), chain));
+steps = 10
+y_forecast = Array(predict(sts_model(T, sts; forecast=steps), chain));
 forecast_plt = plot(1:length(y)+steps, y_forecast', color=1, alpha=0.1, label=nothing, title="Posterior forecast")
 scatter!(forecast_plt, tt, y, label="Data", color=seasons)
