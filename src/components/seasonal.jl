3ce724fcace8121928d8655e3bcaec67059de9bc
Sebastian Callh <sebastian.callh@gmail.com>
2022-07-31 23:35:58 +0200

    Return mapping matrices from components

--- src/components/seasonal.jl
+++ src/components/seasonal.jl
@@ -1,8 +1,8 @@
 struct Seasonal <: Component
     obs::Matrix{Int64}
     trans::Matrix{Int64}
+    trans_cov::Diagonal{Float64}
     season_length::Int64
-    drift_scale::Float64
 end
 
 """
@@ -13,16 +13,17 @@ end
 Seasonal(num_seasons::Integer, season_length::Integer, drift_scale::Real) = begin
     obs = Matrix(vcat(1, zeros(Int64, num_seasons-1))')
     trans = diagm(ones(num_seasons))[:,vcat(num_seasons, 1:num_seasons-1)]
-    Seasonal(obs, trans, season_length, drift_scale)
+    trans_cov = Diagonal(vcat(drift_scale^2, zeros(num_seasons-1)))
+    Seasonal(obs, trans, trans_cov, season_length)
 end
 latent_size(m::Seasonal) = size(m.obs, 2)
 observed_size(m::Seasonal) = size(m.obs, 1)
 
 
-function (m::Seasonal)(x::AbstractArray{T}, t::Integer) where T <: Real
+function (m::Seasonal)(t::Integer)
    num_seasons = size(m.trans, 1)
-   new_season = mod(t, m.season_length) == 0 && 1 < t
-   trans = new_season ? m.trans : diagm(ones(num_seasons))
-   drift_scale = new_season ? vcat(m.drift_scale, zeros(num_seasons-1)) : zeros(num_seasons)
-   m.obs*x, trans*x, drift_scale
+   new_season = mod(t-1, m.season_length) == 0 && 1 < t   
+   trans = new_season ? m.trans : diagm(ones(eltype(m.trans), num_seasons))
+   trans_cov = new_season ? m.trans_cov : Diagonal(zeros(eltype(m.trans_cov), num_seasons))
+   m.obs, trans, trans_cov
 end
--- src/components/sum.jl
+++ src/components/sum.jl
@@ -12,17 +12,10 @@ observed_size(m::Sum) = mapreduce(observed_size, +, m.components)
 Base.length(m::Sum) = length(m.components)
 Base.:(+)(c1::Component, c2::Component) = Sum(vcat(c1, c2))
 
-function (m::Sum)(x::AbstractArray{T}, t::Integer) where T <: Real
-    i = 1
-    results = map(m.components) do c
-        l = latent_size(c)
-        result = c(x[i:i+l-1], t)
-        i += l
-        result
-    end
-
-    eff = mapreduce(r -> r[1], +, results)
-    x′ = mapreduce(r -> r[2], vcat, results)
-    ϵ = mapreduce(r -> r[3], vcat, results)
-    eff, x′, ϵ
+function (m::Sum)(t::Integer)
+    results = map(c -> c(t), m.components)
+    obs = mapreduce(r -> r[1], hcat, results)
+    trans = blockdiagonal([r[2] for r in results])
+    trans_cov = blockdiagonal([r[3] for r in results])
+    obs, trans, trans_cov
 end
