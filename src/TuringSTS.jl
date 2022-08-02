4cf1a7d5d88427745b589bedb3024722e160d8a5
Sebastian Callh <sebastian.callh@gmail.com>
2022-08-02 22:52:03 +0200

    Add local level model

--- src/TuringSTS.jl
+++ src/TuringSTS.jl
@@ -11,6 +11,7 @@ export kalman, predictive
 include("components/utils.jl")
 include("components/component.jl")
 include("components/seasonal.jl")
+include("components/local_level.jl")
 include("components/local_linear.jl")
 include("components/sum.jl")
 
