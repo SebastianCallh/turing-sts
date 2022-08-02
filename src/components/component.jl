0678108eba766ee2e55e737221e691d02c7d3f19
Sebastian Callh <sebastian.callh@gmail.com>
2022-08-02 22:51:00 +0200

    Add jitter to component simulate

--- src/components/component.jl
+++ src/components/component.jl
@@ -6,13 +6,13 @@ abstract type Component end
 Simulates from the provided `model` for `T` time steps
 with initial state `x` and observation noise `σ`.
 """
-function simulate(model::Component, T, x, σ)
-    mapreduce(hcat, 1:T) do t
-        obs, trans, Σx = model(t)
-        x = rand(MvNormal(trans*x, Σx))
-
-        μy = obs*x
-        Σy = σ^2 .* I(size(μy, 1))
-        y = rand(MvNormal(μy, Σy))
+function simulate(sts::Component, T, x, σ; jitter=1e-8)
+    res = mapreduce(hcat, 1:T) do t
+        H, F, Q = sts(t)
+        latent_dim = size(F, 1)
+        x = rand(MvNormal(F*x, Q .+ jitter .* abs.(diagm(randn(latent_dim)))))
+        y = H*x
+        (;x, y)
     end
-end
+    mapreduce(r -> r.x, hcat, res), mapreduce(r -> r.y, hcat, res)
+end 
