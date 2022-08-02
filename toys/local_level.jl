c825ae6140fa98d95efc842e90eb96539badb4d3
Sebastian Callh <sebastian.callh@gmail.com>
2022-08-02 23:20:22 +0200

    Update local level toy example for kalman

--- toys/local_level.jl
+++ toys/local_level.jl
@@ -1,62 +1,55 @@
-using StatsPlots, Turing, Distributions, LinearAlgebra, Statistics
-using ReverseDiff, Memoization
+using StatsPlots
+using Turing
+using Distributions
+using LinearAlgebra
+using Statistics
 using Random
 
-Random.seed!(1234)
-Turing.setadbackend(:reversediff)
-Turing.setrdcache(true)
+using TuringSTS
 
-function true_fn(t, α, ϵ, σ)
-    map(t) do _
-        y = α + σ*randn()
-        α += ϵ*randn()    
-        y  
-    end
-end
+##
+Random.seed!(12345)
 
-N = 50
-σ = 0.1
-t = collect(1:N)
-α₀ = 0.3
-ϵ = 0.1
-y = Float32.(true_fn(t, α₀, ϵ, σ))
-scatter(t, y)
+local_level_init = [-0.8]
+x₀ = vcat(local_level_init)
+T = 5
+σ = 0.01
+tt = collect(1:T)
+num_test = 5
 
-@model function local_level(T, type, forecast=0)
-    ϵ ~ Gamma(2, .1)
-    α_z ~ MvNormal(zeros(T-1), I)
-    α₀ ~ Normal(0, 1)
-    σ ~ Exponential(0.1)
+level_drift_scale = 0.1
+sts = LocalLevel(level_drift_scale)
 
-    T′ = T + forecast
-    α = Vector{type}(undef, T′)
-    α[1] = α₀
-    for t in 2:T′
-        α[t] = α[t-1] + ϵ*α_z[min(T-1, t-1)]
-    end
+y, _ = simulate(sts, T + num_test, x₀, σ)
+y_test = y[:,T+1:end]
+y = y[:,1:T]
+tt_test = tt[end]+1:tt[end]+num_test
 
-    μ = α
-    Σ = I*σ^2
-    y ~ MvNormal(μ, Σ)
+data_plt = scatter(tt, y', label=nothing, color=1, title="Train data")
+scatter!(data_plt, tt_test, y_test', label=nothing, color=2, title="Test data")
 
-    return (;α, ϵ, σ)
-end
+##
+prior_model = kalman(sts, y)
+prior_chain = sample(prior_model, Prior(), 100)
+prior_μ, prior_Σ = predictive(prior_model, prior_chain)
+prior_plt = plot(tt, mean(prior_μ; dims=2), ribbon=mean(prior_Σ; dims=2), label=nothing, title="Prior predictive")
+scatter!(prior_plt, tt, y', label="Data")
 
-model = local_level(N, eltype(y))
-prior_samples = mapreduce(i -> rand(model).y, hcat, 1:50)
-prior_plt = plot(t, prior_samples, label=nothing, color=1, title="Prior predictive check")
-scatter!(prior_plt, t, y, color=2, label="Data")
+##
+Random.seed!(1)
+model = kalman(sts, y)
+chain = sample(model, NUTS(), 1000)
+post_μ, post_Σ = predictive(model, chain)
+@assert isapprox(mean(chain, "x₀[1]"), x₀[1]; atol=0.25)
 
-chain = sample(model | (;y), NUTS(), 1000)
-posterior_samples = predict(model, chain)
-@assert isapprox(mean(chain, :σ), σ; atol=0.1)
-@assert isapprox(mean(chain, :ϵ), ϵ; atol=0.1)
-@assert isapprox(mean(chain, :α₀), α₀; atol=0.1)
+## 
+post_μ, post_Σ = predictive(model, chain)
+post_plt = plot(tt, mean(post_μ; dims=2), ribbon=mean(post_Σ; dims=2), label=nothing, title="Prior predictive")
+scatter!(post_plt, tt, y', label="Data")
 
-posterior_plt = plot(t, Array(posterior_samples)', color=1, alpha=0.1, label=nothing, title="Posterior predictive check")
-scatter!(posterior_plt, t, y, label="Data", color=2)
-
-steps = 10
-y_forecast = Array(group(predict(local_level(N, eltype(y), steps), chain), :y))
-forecast_plt = plot(1:length(y)+steps, y_forecast', color=1, alpha=0.1, label=nothing, title="Posterior forecast")
-scatter!(forecast_plt, t, y, label="Data", color=2)
+## 
+steps = num_test
+forecast_μ, forecast_Σ = predictive(kalman(sts, y; forecast=steps), chain)
+forecast_plt = plot(1:length(y)+steps, mean(forecast_μ; dims=2), ribbon=mean(forecast_Σ; dims=2), label=nothing, title="Posterior forecast")
+scatter!(forecast_plt, tt, y', label="Train data")
+scatter!(forecast_plt, tt[end]+1:tt[end]+num_test, y_test', label="Test data", color="gray")
